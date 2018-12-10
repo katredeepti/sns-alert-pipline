f6748d5df9759a80b70f26d8160d76dbf9a11139
katredeepti <45236185+katredeepti@users.noreply.github.com>
2018-12-10 10:12:57 -0800

    Add files via upload
    sns-prod-server-alert

---
+++ sns-alert-pipline/Lambda-sns-cloudwatch-master/LambdaCode/lambda_function.py
@@ -0,0 +1,8 @@
+import boto3
+client = boto3.client('sns')
+
+def lambda_handler(event, context):
+    client.publish(
+    TopicArn='arn:aws:sns:us-east-2:992014891278:prod_stop_notification',
+    Message='prod server is stopped pls action on it',
+    )
