972606eff7d982847be82a6bcd95c9db882b26e7
Deepti Katre <katredeepti@gmail.com>
2019-06-13 13:20:46 +0200

    testing

--- sns-alert-pipline/Lambda-sns-cloudwatch-master/LambdaCode/lambda_function.py
+++ sns-alert-pipline/Lambda-sns-cloudwatch-master/LambdaCode/lambda_function.py
@@ -4,5 +4,5 @@ client = boto3.client('sns')
 def lambda_handler(event, context):
     client.publish(
     TopicArn='arn:aws:sns:us-east-2:992014891278:prod_stop_notification',
-    Message='prod server is stopped pls action on it',
+    Message='prod server is stopped pls action on it please',
     )
